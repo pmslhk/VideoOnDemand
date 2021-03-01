75af9d71b6bcad4623e5ad61a6714a7ee2b64c90
pmslhk <pmslhk@hanmail.net>
2021-03-01 22:06:59 +0900

    초기 커밋.

---
+++ VOD.Database/Contexts/VODContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VOD.Common.Entities;
+
+namespace VOD.Database.Contexts
+{
+    public class VODContext : IdentityDbContext<VODUser>
+    {
+        public VODContext(DbContextOptions<VODContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ VOD.UI/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using VOD.UI.Models;
+using Microsoft.AspNetCore.Identity;
+using VOD.Common.Entities;
+using VOD.UI.Models;
+
+namespace VOD.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SignInManager<VODUser> _signInManager;
+
+        public HomeController(SignInManager<VODUser> _signInMgr)
+        {
+            _signInManager = _signInMgr;
+        }
+
+        public IActionResult Index()
+        {
+            if (!_signInManager.IsSignedIn(User)) return RedirectToPage("/Account/Login", new { Area = "Identity" });
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
