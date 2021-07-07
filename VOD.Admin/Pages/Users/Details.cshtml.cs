19777552443b6ad980985d4e6f3dc0aa52897f83
pmslhk <pmslhk@hanmail.net>
2021-07-07 16:10:08 +0900

    23,24 버튼 오류수정 및 프로젝트 제외및 이동

--- VOD.Admin/Pages/Users/Details.cshtml.cs
+++ VOD.Admin/Pages/Users/Details.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -13,47 +14,47 @@ using VOD.Database.Services;
 
 namespace VOD.Admin.Pages.Users
 {
+    [Authorize(Roles = "Admin")]
     public class DetailsModel : PageModel
     {
-        #region Properties and Variables
+        #region Properties
         private readonly IDbReadService _dbRead;
         private readonly IDbWriteService _dbWrite;
-        public IEnumerable<Course> Courses { get; set; } =
-        new List<Course>();
+        public IEnumerable<Course> Courses { get; set; } = new List<Course>();
         public SelectList AvailableCourses { get; set; }
-
-        [BindProperty, Display(Name = "Available Courses")]
-        public int CourseId { get; set; }
+        [BindProperty, Display(Name = "Available Courses")] public int CourseId { get; set; }
         public UserDTO Customer { get; set; }
         #endregion
 
         #region Constructor
-        public DetailsModel(IDbReadService dbReadService,
-        IDbWriteService dbWriteService)
+        public DetailsModel(IDbReadService dbReadService, IDbWriteService dbWriteService)
         {
             _dbRead = dbReadService;
             _dbWrite = dbWriteService;
         }
         #endregion
+
+        #region Helper Methods
         private async Task FillViewData(string userId)
         {
             // Fetch the user/customer
-            var user = await _dbRead.SingleAsync<VODUser>(u =>
-            u.Id.Equals(userId));
+            var user = await _dbRead.SingleAsync<VODUser>(u => u.Id.Equals(userId));
             Customer = new UserDTO { Id = user.Id, Email = user.Email };
+
             // Fetch the user's courses and course ids
             _dbRead.Include<UserCourse>();
-            var userCourses = await _dbRead.GetAsync<UserCourse>(uc =>
-            uc.UserId.Equals(userId));
-            var usersCourseIds = userCourses.Select(c =>
-            c.CourseId).ToList();
+            var userCourses = await _dbRead.GetAsync<UserCourse>(uc => uc.UserId.Equals(userId));
+            var usersCourseIds = userCourses.Select(c => c.CourseId).ToList();
             Courses = userCourses.Select(c => c.Course).ToList();
+
             // Fetch courses that the user doesn't already have access to
-            var availableCourses = await _dbRead.GetAsync<Course>(uc =>
-            !usersCourseIds.Contains(uc.Id));
+            var availableCourses = await _dbRead.GetAsync<Course>(uc => !usersCourseIds.Contains(uc.Id));
             AvailableCourses = availableCourses.ToSelectList("Id", "Title");
         }
-        public async Task OnGetASync(string id)
+        #endregion
+
+        #region Actions
+        public async Task OnGetAsync(string id)
         {
             await FillViewData(id);
         }
@@ -69,20 +70,20 @@ namespace VOD.Admin.Pages.Users
                 var succeeded = await _dbWrite.SaveChangesAsync();
             }
             catch
-
             {
             }
+
             await FillViewData(userId);
             return Page();
         }
-        public async Task<IActionResult> OnPostRemoveAsync(int courseId,
-        string userId)
+        public async Task<IActionResult> OnPostRemoveAsync(int courseId, string userId)
         {
             try
             {
                 var userCourse = await _dbRead.SingleAsync<UserCourse>(uc =>
-                uc.UserId.Equals(userId) &&
-                uc.CourseId.Equals(courseId));
+                    uc.UserId.Equals(userId) &&
+                    uc.CourseId.Equals(courseId));
+
                 if (userCourse != null)
                 {
                     _dbWrite.Delete(userCourse);
@@ -92,10 +93,11 @@ namespace VOD.Admin.Pages.Users
             catch
             {
             }
+
             await FillViewData(userId);
             return Page();
         }
-
+        #endregion
     }
-
 }
+
