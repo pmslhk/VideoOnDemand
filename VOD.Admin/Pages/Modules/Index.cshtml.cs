e94f4731e6c21bc1616a33f990805305d8719768
pmslhk <pmslhk@hanmail.net>
2021-07-14 15:51:10 +0900

    24-3

--- VOD.Admin/Pages/Modules/Index.cshtml.cs
+++ VOD.Admin/Pages/Modules/Index.cshtml.cs
@@ -1,15 +1,11 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using VOD.Common.DTOModels.Admin;
+using VOD.Common.Entities;
 using VOD.Common.Services;
-//using VOD.Database.Services;
-
 
 namespace VOD.Admin.Pages.Modules
 {
@@ -17,24 +13,20 @@ namespace VOD.Admin.Pages.Modules
     public class IndexModel : PageModel
     {
         #region Properties
-        
-        public IEnumerable<ModuleDTO> Items = new List<ModuleDTO>();
-
-        [TempData] 
-        public string Alert { get; set; }
-        #endregion
-        #region Constructor
-
         private readonly IAdminService _db;
+        public IEnumerable<ModuleDTO> Items = new List<ModuleDTO>();
+        [TempData] public string Alert { get; set; }
+
+        #endregion
+
+        #region Constructor
         public IndexModel(IAdminService db)
         {
             _db = db;
         }
-
         #endregion
 
         public async Task<IActionResult> OnGetAsync()
-
         {
             try
             {
@@ -42,16 +34,10 @@ namespace VOD.Admin.Pages.Modules
                 return Page();
             }
             catch
-
             {
                 Alert = "You do not have access to this page.";
                 return RedirectToPage("/Index");
             }
         }
-
-
     }
-
-
 }
-
